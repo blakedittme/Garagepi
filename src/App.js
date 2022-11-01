79bb8db58dad1964ee4542d60352972c3a0e364a
blakedittme <bdittme@outlook.com>
2022-10-31 21:24:45 -0500

    initial page for making request to pi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+
+const App = () => {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState();
+  const [err, setErr] = useState('');
+
+  const handleClick = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch('enter our endpoint here', {
+        method: 'POST',
+        body: JSON.stringify({
+          name: 'blake',
+          job: 'software dev',
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error status: ${response.status}`);
+      }
+
+      const result = await response.json();
+
+      console.log('result is: ', JSON.stringify(result, null, 4));
+
+      setData(result);
+    } catch (err) {
+      setErr(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {err && <h2>{err}</h2>}
+
+      <button onClick={handleClick}>Open Garage</button>
+
+      {isLoading && <h2>Loading...</h2>}
+
+      {data && (
+        <div>
+          <h2>Name: {data.name}</h2>
+          <h2>Job: {data.job}</h2>
+        </div>
+      )}
     </div>
   );
 }
+
+
 
 export default App;
